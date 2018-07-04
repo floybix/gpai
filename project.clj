b06db4365477f0193f3547ae457028ad2eacb093
Felix Andrews <felix@nfrac.org>
2018-07-04 13:43:56 +1000

    Use tools.deps

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://github.com/floybix/gpai"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.generators "0.1.2"]]
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
   :jvm-opts ^:replace [])
